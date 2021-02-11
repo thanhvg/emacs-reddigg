7417ae4402c12ae823f19f3e1876be609566ce78
Thanh Vuong <thanhvg@gmail.com>
2021-02-11 00:02:06 -0700

    01

--- reddigg.el
+++ reddigg.el
@@ -30,7 +30,7 @@
   (if (and (functionp 'json-parse-buffer)
            ;; json-parse-buffer only supports keyword arguments in Emacs 27+
            (>= emacs-major-version 27))
-        #'json-parse-buffer
+      #'json-parse-buffer
     #'json-read)
   "Function to use to parse JSON strings.")
 
@@ -42,22 +42,79 @@
 
 
 (defun reddigg--get-post (subreddit)
- (promise-new
-  (lambda (resolve reject)
-    (request (format reddigg--sub-url 'acmilan)
-      :headers `(("User-Agent" . "fun"))
-      :parser 'reddigg--json-parser-fn
-      ;; :parser 'json-read
-      :error (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
-                            (funcall reject  error-thrown)))
-      :success (cl-function (lambda (&key data &allow-other-keys)
-                              (funcall resolve data)))))))
+  (promise-new
+   (lambda (resolve reject)
+     (request (format reddigg--sub-url subreddit)
+       :headers `(("User-Agent" . "fun"))
+       :parser 'reddigg--json-parser-fn
+       ;; :parser 'json-read
+       :error (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
+                             (funcall reject  error-thrown)))
+       :success (cl-function (lambda (&key data &allow-other-keys)
+                               (funcall resolve data)))))))
 
-(promise-chain (reddigg--get-post 'acmilan)
+(promise-chain (reddigg--get-post 'emacs)
   (then (lambda (result)
-          (setq thanh result)))
+          ;; (setq thanh result)
+          (gethash "children" (gethash "data" result))))
+  (then #'reddigg--print-subreddit)
   (promise-catch (lambda (reason)
                    (message "catch error in promise: %s" reason))))
 
 ;; (gethash "data" thanh)
-(gethash "children" (gethash "data" thanh))
+(setq thanh-3 (gethash "children" (gethash "data" thanh)))
+(vectorp thanh-3)
+(setq thanh-4 (aref thanh-3 0))
+
+(gethash "url_overridden_by_dest" (gethash "data" thanh-4))
+(gethash "author_fullname" (gethash "data" thanh-4))
+(gethash "author" (gethash "data" thanh-4))
+(gethash "ups" (gethash "data" thanh-4))
+(gethash "score" (gethash "data" thanh-4))
+(gethash "num_comments" (gethash "data" thanh-4))
+(gethash "url" (gethash "data" thanh-4))
+(gethash "title" (gethash "data" thanh-4))
+(gethash "permalink" (gethash "data" thanh-4))
+
+
+(defvar reddigg--buffer "*reddigg*"
+  "Buffer for main page.")
+
+(defvar reddigg--cmt-buffer "*reddigg-comments*"
+  "Comment buffer.")
+
+(defun reddigg--get-buffer ()
+  (get-buffer-create reddigg--buffer))
+
+(defun reddigg--get-cmt-buffer ()
+  (get-buffer-create reddigg--cmt-buffer))
+
+(defun reddigg--print-subreddit (data)
+  (with-current-buffer (reddigg--get-buffer)
+    (erase-buffer)
+    (insert "#+startup: overview indent\n")
+    (insert "#+title: subreddit\n")
+    (seq-do
+     (lambda (it)
+       (let ((my-it (gethash "data" it)))
+         (insert "* " (gethash "title" my-it) "\n")
+         (insert "score: " (format "%s" (gethash "score" my-it) ) "\n")
+         (insert "comments: " (format "%s" (gethash "num_comments" my-it)) "\n")
+         (let ((selftext (gethash "selftext" my-it)) begin end)
+           (if (string-empty-p selftext)
+               (insert "url: " (gethash "url" my-it) "\n")
+             (setq begin (point))
+             (insert "\n" selftext "\n")
+             (setq end (point))
+             (reddigg--sanitize-range begin end)))))
+     data)))
+
+(defun reddigg--sanitize-range (begin end)
+  (save-excursion
+    (goto-char begin)
+    (while (re-search-forward "^\\* " end t)
+      (replace-match "- "))))
+
+(if (string-empty-p "") (message "yay"))
+(reddigg--print-subreddit thanh-3)
+(switch-to-buffer (reddigg--get-buffer))
